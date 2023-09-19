e47d5a1d93ee191602b43e2ca3190e0e6d76b058
adeguzman2 <adeguzman@airasia.com>
2023-09-19 19:22:19 +0800

    FEAT: implement counter app and user list

---
+++ frontend/src/components/Counters/Counters.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "../Counter/Counter";
+
+const Counters = () => {
+  return (
+    <>
+      <Counter />
+      <Counter />
+    </>
+  );
+};
+
+export default Counters;
