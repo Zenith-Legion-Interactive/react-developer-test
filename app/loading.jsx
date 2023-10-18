902a49a57aa0e8fcd08db891574ea120ad2e530a
jhynzar <jhynzar@gmail.com>
2023-10-18 15:02:37 +0800

    [Task 3] API Integration [UI, API]

---
+++ app/loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="h-full flex justify-center items-center">
+        <h2>Loading...</h2>
+      </div>
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-'use client'
-
+import { Suspense } from "react";
 import Counter from "./Counter";
+import UserList from "./UserList";
+
+import Loading from './loading';
 
 export default function Home() {
   return (
     <>
       <div className="h-screen flex flex-col justify-center items-around">
         <div>
-          <Counter />
-        </div>
-        <div>
-          <Counter />
+          <Suspense fallback={<Loading />}>
+              <UserList />
+          </Suspense>
         </div>
       </div>
     </>
