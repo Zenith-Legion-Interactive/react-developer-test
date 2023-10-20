c2ff09c7844f7b4fc46e28f9ea5f8b6bd23664c4
jhynzar <jhynzar@gmail.com>
2023-10-20 11:10:34 +0800

    [Task 5][Config] Tailwind - Added Theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,32 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        // Main Theme
+        'blue-purple': '#9DA7D3',
+        'blue-dark': '#0D182F',
+        'blue': '#023E8B',
+        'blue-light': '#1964B0',
+        'pink-darker': '#D86E6E',
+        'pink-dark': '#E18E8E',
+        'pink': '#E9ADAD',
+        // Supporting
+        'orange': '#ffa900', // warning
+        'red-dark': '#912f40', // danger-dark
+        'red': '#BA1F33', // danger
+        'green-dark': '#0C7C59', //success-dark
+        'green': '#7DDF64' //success
+      },
     },
+   
+    
   },
   plugins: [],
+  safelist: [
+    {
+      pattern:
+        /(bg|text|border)-(blue-purple|blue-dark|blue|blue-light|pink-darker|pink-dark|pink|orange|red-dark|red|green-dark|green)/,
+    },
+  ],
 }
 export default config
